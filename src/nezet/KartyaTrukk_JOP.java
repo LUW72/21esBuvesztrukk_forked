4fc2f13ac74be190895d1da88255515d9b681ff0
LUW72 <schneider.ivan337@gmail.com>
2025-09-18 14:26:32 +0200

    GUI kezdetek

--- src/nezet/KartyaTrukk_JOP.java
+++ src/nezet/KartyaTrukk_JOP.java
@@ -25,7 +25,7 @@ public class KartyaTrukk_JOP
         
         for (int i = 0; i < 3; i++) 
         {              
-            int oszlop = felugroraIr(oszlopok()+ SEP + pakli.kirak()+SEP);            
+            int oszlop = felugroraIrGombokkal(oszlopok()+ SEP + pakli.kirak()+SEP);            
 
             this.pakli.kever(oszlop);
         }
@@ -44,15 +44,32 @@ public class KartyaTrukk_JOP
     
     private int felugroraIr(String s)
     {
-        String formatted = "<html><pre>" + s + "\nMelyik oszlopban van? (1-3)</pre></html>";
-        String input = JOptionPane.showInputDialog(null, formatted);
+        String input = JOptionPane.showInputDialog(null, "<html><pre>" + s + "\nMelyik oszlopban van? (1-3)</pre></html>");
         return Integer.parseInt(input.trim());
-    }
-   
+    }    
     
+    private int felugroraIrGombokkal(String s) 
+    {
+        // thx Lord GPTron Prime
+        String formatted = "<html><pre>" + s + "\nMelyik oszlopban van?</pre></html>";
+
+        Object[] options = {"1", "2", "3"};
+
+        int choice = JOptionPane.showOptionDialog( null, formatted, "Kártya választás", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+
+        if (choice == JOptionPane.CLOSED_OPTION) 
+        {
+            JOptionPane.showMessageDialog(null, "A program leállt.", "Kilépés", JOptionPane.INFORMATION_MESSAGE);
+            System.exit(0);
+        }
+
+        return choice + 1;
+    }
+
 
     private void melyikVolt() 
     {
-        JOptionPane.showConfirmDialog(null, "A választott kártya a(z) %s volt!".formatted(pakli.getLap(11).getLeiras()));
+        JOptionPane.showMessageDialog(null, "<html><h2>" + "A gondolt lap a(z):<br><br>" + "<center>"+ pakli.getLap(11).getLeiras() + "</center>" + "</h2></html>", "Eredmény", JOptionPane.INFORMATION_MESSAGE);
     }
+
 }
